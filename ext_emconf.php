d09418add7022e13aae4d6419cec26e976dcb9cf
Alexander Kuemmel <ak@alexanderkuemmel.com>
2013-04-02 01:18:22 +0200

    initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+  'title' => 'akisysproxigal',
+  'description' => '',
+  'category' => 'plugin',
+  'author' => 'Alexander Kuemmel',
+  'author_email' => 'akisys@alexanderkuemmel.com',
+  'shy' => '',
+  'dependencies' => '',
+  'conflicts' => '',
+  'priority' => '',
+  'module' => '',
+  'state' => 'alpha',
+  'internal' => '',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'modify_tables' => '',
+  'clearCacheOnLoad' => 0,
+  'lockType' => '',
+  'author_company' => 'Akisys',
+  'version' => '0.0.0',
+  'constraints' => array(
+    'depends' => array(
+    ),
+    'conflicts' => array(
+    ),
+    'suggests' => array(
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"a22f";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"7205";s:14:"ext_tables.php";s:4:"708c";s:14:"ext_tables.sql";s:4:"d41d";s:16:"locallang_db.xml";s:4:"9ea9";s:10:"README.txt";s:4:"ee2d";s:40:"Resources/Private/Language/locallang.xml";s:4:"3d2d";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"ecde";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:14:"doc/manual.sxw";s:4:"8d2d";s:19:"doc/wizard_form.dat";s:4:"6fd9";s:20:"doc/wizard_form.html";s:4:"2008";s:35:"pi1/class.tx_akisysproxigal_pi1.php";s:4:"d4cc";}',
+);
+
+?>
