d44c7c3592e157b661516d351c926dc804ad522f
Alexander Kuemmel <ak@alexanderkuemmel.com>
2013-04-17 09:41:33 +0200

    added default fluid icon

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "akisysproxigal".
  *
- * Auto generated 17-04-2013 09:37
+ * Auto generated 17-04-2013 09:41
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"f619";s:34:"Configuration/TypoScript/setup.txt";s:4:"ca3c";s:48:"Resources/Private/Elements/ProximityGallery.html";s:4:"e2c5";s:40:"Resources/Private/Language/locallang.xml";s:4:"4c77";s:31:"Resources/Public/css/master.css";s:4:"beb5";s:42:"Resources/Public/css/colorbox/colorbox.css";s:4:"1e4b";s:47:"Resources/Public/css/colorbox/images/border.png";s:4:"7ca8";s:49:"Resources/Public/css/colorbox/images/controls.png";s:4:"b68b";s:48:"Resources/Public/css/colorbox/images/loading.gif";s:4:"e661";s:59:"Resources/Public/css/colorbox/images/loading_background.png";s:4:"acf4";s:48:"Resources/Public/css/colorbox/images/overlay.png";s:4:"7903";s:63:"Resources/Public/css/colorbox/images/ie6/borderBottomCenter.png";s:4:"1936";s:61:"Resources/Public/css/colorbox/images/ie6/borderBottomLeft.png";s:4:"7cee";s:62:"Resources/Public/css/colorbox/images/ie6/borderBottomRight.png";s:4:"297f";s:61:"Resources/Public/css/colorbox/images/ie6/borderMiddleLeft.png";s:4:"64df";s:62:"Resources/Public/css/colorbox/images/ie6/borderMiddleRight.png";s:4:"9fa4";s:60:"Resources/Public/css/colorbox/images/ie6/borderTopCenter.png";s:4:"01ec";s:58:"Resources/Public/css/colorbox/images/ie6/borderTopLeft.png";s:4:"bf49";s:59:"Resources/Public/css/colorbox/images/ie6/borderTopRight.png";s:4:"5131";s:37:"Resources/Public/js/akisysproxigal.js";s:4:"c37c";s:42:"Resources/Public/js/jquery.colorbox-min.js";s:4:"3f99";s:39:"Resources/Public/js/jquery.proximity.js";s:4:"9dbc";}',
+	'_md5_values_when_last_written' => 'a:23:{s:12:"ext_icon.gif";s:4:"e922";s:14:"ext_tables.php";s:4:"f619";s:34:"Configuration/TypoScript/setup.txt";s:4:"ca3c";s:48:"Resources/Private/Elements/ProximityGallery.html";s:4:"e2c5";s:40:"Resources/Private/Language/locallang.xml";s:4:"4c77";s:31:"Resources/Public/css/master.css";s:4:"beb5";s:42:"Resources/Public/css/colorbox/colorbox.css";s:4:"1e4b";s:47:"Resources/Public/css/colorbox/images/border.png";s:4:"7ca8";s:49:"Resources/Public/css/colorbox/images/controls.png";s:4:"b68b";s:48:"Resources/Public/css/colorbox/images/loading.gif";s:4:"e661";s:59:"Resources/Public/css/colorbox/images/loading_background.png";s:4:"acf4";s:48:"Resources/Public/css/colorbox/images/overlay.png";s:4:"7903";s:63:"Resources/Public/css/colorbox/images/ie6/borderBottomCenter.png";s:4:"1936";s:61:"Resources/Public/css/colorbox/images/ie6/borderBottomLeft.png";s:4:"7cee";s:62:"Resources/Public/css/colorbox/images/ie6/borderBottomRight.png";s:4:"297f";s:61:"Resources/Public/css/colorbox/images/ie6/borderMiddleLeft.png";s:4:"64df";s:62:"Resources/Public/css/colorbox/images/ie6/borderMiddleRight.png";s:4:"9fa4";s:60:"Resources/Public/css/colorbox/images/ie6/borderTopCenter.png";s:4:"01ec";s:58:"Resources/Public/css/colorbox/images/ie6/borderTopLeft.png";s:4:"bf49";s:59:"Resources/Public/css/colorbox/images/ie6/borderTopRight.png";s:4:"5131";s:37:"Resources/Public/js/akisysproxigal.js";s:4:"c37c";s:42:"Resources/Public/js/jquery.colorbox-min.js";s:4:"3f99";s:39:"Resources/Public/js/jquery.proximity.js";s:4:"9dbc";}',
 	'suggests' => array(
 	),
 );
