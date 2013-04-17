00963d0bce223f2a99f20017353b020c2a80babe
Alexander Kuemmel <ak@alexanderkuemmel.com>
2013-04-17 09:38:28 +0200

    updated TYPO3 repo infos

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,34 +1,48 @@
 <?php
 
+/***************************************************************
+ * Extension Manager/Repository config file for ext "akisysproxigal".
+ *
+ * Auto generated 17-04-2013 09:37
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
 $EM_CONF[$_EXTKEY] = array(
-  'title' => 'akisysproxigal',
-  'description' => '',
-  'category' => 'plugin',
-  'author' => 'Alexander Kuemmel',
-  'author_email' => 'akisys@alexanderkuemmel.com',
-  'shy' => '',
-  'dependencies' => '',
-  'conflicts' => '',
-  'priority' => '',
-  'module' => '',
-  'state' => 'alpha',
-  'internal' => '',
-  'uploadfolder' => 0,
-  'createDirs' => '',
-  'modify_tables' => '',
-  'clearCacheOnLoad' => 0,
-  'lockType' => '',
-  'author_company' => 'Akisys',
-  'version' => '0.0.0',
-  'constraints' => array(
-    'depends' => array(
-    ),
-    'conflicts' => array(
-    ),
-    'suggests' => array(
-    ),
-  ),
-  '_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"a22f";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"7205";s:14:"ext_tables.php";s:4:"708c";s:14:"ext_tables.sql";s:4:"d41d";s:16:"locallang_db.xml";s:4:"9ea9";s:10:"README.txt";s:4:"ee2d";s:40:"Resources/Private/Language/locallang.xml";s:4:"3d2d";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"ecde";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:14:"doc/manual.sxw";s:4:"8d2d";s:19:"doc/wizard_form.dat";s:4:"6fd9";s:20:"doc/wizard_form.html";s:4:"2008";s:35:"pi1/class.tx_akisysproxigal_pi1.php";s:4:"d4cc";}',
+	'title' => 'Akisys: Proximity Image Gallery',
+	'description' => 'Creates a grid-based image gallery with proximity-based thumbnail hovers and an integrated lightbox.',
+	'category' => 'plugin',
+	'author' => 'Alexander Kuemmel',
+	'author_email' => 'akisys@alexanderkuemmel.com',
+	'shy' => '',
+	'dependencies' => 'fluidcontent,t3jquery',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'Akisys',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'fluidcontent' => '2.2.2-0.0.0',
+			't3jquery' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:23:{s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"f619";s:34:"Configuration/TypoScript/setup.txt";s:4:"ca3c";s:48:"Resources/Private/Elements/ProximityGallery.html";s:4:"e2c5";s:40:"Resources/Private/Language/locallang.xml";s:4:"4c77";s:31:"Resources/Public/css/master.css";s:4:"beb5";s:42:"Resources/Public/css/colorbox/colorbox.css";s:4:"1e4b";s:47:"Resources/Public/css/colorbox/images/border.png";s:4:"7ca8";s:49:"Resources/Public/css/colorbox/images/controls.png";s:4:"b68b";s:48:"Resources/Public/css/colorbox/images/loading.gif";s:4:"e661";s:59:"Resources/Public/css/colorbox/images/loading_background.png";s:4:"acf4";s:48:"Resources/Public/css/colorbox/images/overlay.png";s:4:"7903";s:63:"Resources/Public/css/colorbox/images/ie6/borderBottomCenter.png";s:4:"1936";s:61:"Resources/Public/css/colorbox/images/ie6/borderBottomLeft.png";s:4:"7cee";s:62:"Resources/Public/css/colorbox/images/ie6/borderBottomRight.png";s:4:"297f";s:61:"Resources/Public/css/colorbox/images/ie6/borderMiddleLeft.png";s:4:"64df";s:62:"Resources/Public/css/colorbox/images/ie6/borderMiddleRight.png";s:4:"9fa4";s:60:"Resources/Public/css/colorbox/images/ie6/borderTopCenter.png";s:4:"01ec";s:58:"Resources/Public/css/colorbox/images/ie6/borderTopLeft.png";s:4:"bf49";s:59:"Resources/Public/css/colorbox/images/ie6/borderTopRight.png";s:4:"5131";s:37:"Resources/Public/js/akisysproxigal.js";s:4:"c37c";s:42:"Resources/Public/js/jquery.colorbox-min.js";s:4:"3f99";s:39:"Resources/Public/js/jquery.proximity.js";s:4:"9dbc";}',
+	'suggests' => array(
+	),
 );
 
 ?>
